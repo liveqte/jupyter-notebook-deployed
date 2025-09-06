ba689ef00d08cded5eed5e9bd8ebb48cc3a32d6c
liveqte <3571650235@qq.com>
2025-09-06 22:35:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ mkdir -p /tmp/code-server/{config,data,cache,extensions}
 
 # 启动 code-server（假设你已安装在当前目录）
 npx code-server \
+  --host 0.0.0.0 \
   --port 8080 \
   --auth none \
   --user-data-dir /tmp/code-server/data \
