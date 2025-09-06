dcd7d8aa940df8b72f55d2f03071c12e0d0f87de
liveqte <3571650235@qq.com>
2025-09-06 21:00:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p /tmp/code-server/{config,data,cache,extensions}
 
 # 启动 code-server（假设你已安装在当前目录）
-./code-server-4.103.2-linux-amd64/bin/code-server \
+code-server \
   --port 8080 \
   --auth none \
   --user-data-dir /tmp/code-server/data \
